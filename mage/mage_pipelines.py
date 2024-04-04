da7fc59b9e1b233278670743c401b8eadfa16354
Amanda Kershaw <amandalnkershaw@gmail.com>
2024-04-04 17:03:41 -0400

    fix(mage): changed pipeline trigger method from api to schedule:once

--- mage/mage_pipelines.py
+++ mage/mage_pipelines.py
@@ -44,63 +44,73 @@ def status_check(job_id):
 					elapsed_time = time.time() - start_time
 					print(f'....{elapsed_time:>3.0f}s. status: {status}')
 					if status == "completed":
-						print(f'pipeline is complete!')
+						print(f'Pipeline is complete!')
 						return
 					if status == "failed" or status == "cancelled":
-						print(f'pipeline encountered an error. check the logs and try again.')
+						print(f'Pipeline encountered an error. Check the logs and try again.')
 						exit_program()
 		else:
-			print(f'api encountered an error. check the logs and try again.')
+			print(f'Api encountered an error. Check the logs and try again.')
 			exit_program()
 		
 		time.sleep(5)
 
+
 def phase_1():
 	# this phase writes data from api to google cloud storage bucket
-	phase_one_url = "http://localhost:6789/api/pipeline_schedules/1/pipeline_runs/3ffcd4276d7b47cd890e60b828bc633c"
+	phase_one_url = "http://localhost:6789/api/pipeline_schedules/5/pipeline_runs/key"
 	# call the function to make the API call
 	api_data = make_api_call(phase_one_url, "POST")
 	if api_data:
-		print(f"running pipeline 1 with id: {api_data['pipeline_run']['id']}")
+		print(f"Running Phase 1 with id: {api_data['pipeline_run']['id']}")
 		status_check(api_data['pipeline_run']['id'])
-		keep_going = input("you now have api data saved to GCS bucket. do you want to run the second pipeline? (y/n)").lower()
+		keep_going = input("You now have API data saved to your GCS bucket.\nDo you want to run the second pipeline? "
+		                   "(y/n, default: y): ").lower()
+		if len(keep_going) == 0:
+			keep_going = 'y'
 		if keep_going == "y" or keep_going == "yes":
 			phase_2()
 		else:
 			print("exiting.")
 			exit_program()
+
+
 def phase_2():
 	# this phase writes data from google cloud storage bucket to bigquery
-	phase_two_url = "http://localhost:6789/api/pipeline_schedules/2/pipeline_runs/afeb3a6f90a943e0ab61de1c14b41181"
+	phase_two_url = "http://localhost:6789/api/pipeline_schedules/2/pipeline_runs/key"
 	api_data = make_api_call(phase_two_url, "POST")
 	if api_data:
-		print(f"running phase 2 with id: {api_data['pipeline_run']['id']}")
+		print(f"Running Phase 2 with id: {api_data['pipeline_run']['id']}")
 		status_check(api_data['pipeline_run']['id'])
-		keep_going = input("you now have gcs data saved to BigQuery tables. do you want to run the dbt pipeline? (y/n)").lower()
+		keep_going = input(
+			"You now have GCS data saved to BigQuery tables.\n"
+			"Do you want to run the dbt pipeline? (y/n, default: y): ").lower()
+		if len(keep_going) == 0:
+			keep_going = 'y'
 		if keep_going == "y" or keep_going == "yes":
 			phase_3()
 		else:
-			print("exiting.")
+			print("Exiting.")
 			exit_program()
-			
+
+
 def phase_3():
 	# this phase executes the dbt seed and build operations to create BigQuery views
-	phase_three_url = "http://localhost:6789/api/pipeline_schedules/3/pipeline_runs/b6a9d107b2e74927b552bce51e848501"
+	phase_three_url = "http://localhost:6789/api/pipeline_schedules/3/pipeline_runs/key"
 	api_data = make_api_call(phase_three_url, "POST")
 	if api_data:
-		print(f"running phase 3 with id: {api_data['pipeline_run']['id']}")
+		print(f"Running Phase 3 with id: {api_data['pipeline_run']['id']}")
 		status_check(api_data['pipeline_run']['id'])
-		print("you now have views in BigQuery. Mage pipelines are complete!")
+		print("You now have views in BigQuery. Mage pipelines are complete!")
 		exit_program()
 
 
 def main():
-
-	phase_number = input("""Which pipeline do you want to run?
-1) api to gcs folder
-2) gcs folder to bigquery
-3) dbt build
-	""").strip(" ")
+	phase_number = int(input("""Which pipeline do you want to run?
+1) Phase 1 : Api to GCS folder
+2) Phase 2 : GCS folder to BigQuery
+3) Phase 3 : dbt build
+""").strip(" "))
 	if phase_number == 1:
 		phase_1()
 	
@@ -109,11 +119,11 @@ def main():
 	
 	if phase_number == 3:
 		phase_3()
-		
+	
 	else:
-		print("no pipeline selected. exiting.")
+		print("No pipeline selected. Exiting.")
 		exit_program()
 
-		
+
 if __name__ == "__main__":
 	main()
